3fd6abdb55df7be2c22f373834a889ad33b1fd31
Nikxo <razermenyt@gmail.com>
2021-06-29 23:50:43 +0200

    first commit

---
+++ streaming/auth/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded());
+
+app.post("/auth", function (req, res) {
+  /* This server is only available to nginx */
+  const streamkey = req.body.key;
+
+  /* You can make a database of users instead :) */
+  if (streamkey === "0349561075051512636656952") {
+    res.status(200).send();
+    return;
+  }
+  /* Reject the stream */
+  res.status(403).send();
+});
+
+app.listen(8000, function () {
+  console.log("Listening on port 8000!");
+});
