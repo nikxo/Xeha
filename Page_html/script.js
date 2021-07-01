325756695a8709a1bc6e463df4a9c79f6888dd28
Nikxo <razermenyt@gmail.com>
2021-07-02 00:42:09 +0200

    mise a jour de l'interface by veve, update fonction stream

--- Page_html/script.js
+++ Page_html/script.js
@@ -1,70 +1,69 @@
-    // Arrows sidebar events
-    let arrow = document.querySelectorAll(".arrow");
-    for (var i = 0; i < arrow.length; i++) {
-      arrow[i].addEventListener("click", (e) => {
-        let arrowParent = e.target.parentElement.parentElement;
-        arrowParent.classList.toggle("showMenu");
-      });
-    }
+// Arrows sidebar events
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e) => {
+  let arrowParent = e.target.parentElement.parentElement;
+  arrowParent.classList.toggle("showMenu");
+  });
+}
 
-    // Sidebar closed event
-    let sidebar = document.querySelector(".sidebar");
-    let responsive_right_pannel = document.querySelector(".responsive-right-pannel");
-    let sidebarBtn = document.querySelector(".logo-details a i img");
-    console.log(sidebarBtn);
-    sidebarBtn.addEventListener("click", () => {
-      sidebar.classList.toggle("close");
-      responsive_right_pannel.classList.toggle("sidebarclosed");
-    });
+// Sidebar closed event
+let sidebar = document.querySelector(".sidebar");
+let responsive_right_pannel = document.querySelector(".responsive-right-pannel");
+let sidebarBtn = document.querySelector(".logo-details a i img");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+  responsive_right_pannel.classList.toggle("sidebarclosed");
+});
 
-    // Dynamic Right pannel Iframe source
+// Dynamic Right pannel Iframe source
+async function buyNavButtonClick() {
+    document.getElementById("right_pannel").src = "https://quickswap.exchange/#/swap";
+};
+  
+async function sellNavButtonClick() {
+  document.getElementById("right_pannel").src = "https://quickswap.exchange/#/pool";
+};
+  
+async function rewardNavButtonClick() {
+  document.getElementById("right_pannel").src = "https://quickswap.exchange/#/quick";
+};
 
-    async function aclick() {
-      document.getElementById("right_pannel").src = "https://quickswap.exchange/#/swap"
-      console.log("Buy page");
-    };
+async function overviewNavButtonClick() {
+  document.getElementById("right_pannel").src = "https://info.uniswap.org/#/";
+};
 
-    async function bclick() {
-      document.getElementById("right_pannel").src = "https://quickswap.exchange/#/pool"
-      console.log("Sell page");
-    };
+async function tokenNavButtonClick() {
+  document.getElementById("right_pannel").src = "https://info.uniswap.org/#/tokens";
+};
 
-    async function cclick() {
-      document.getElementById("right_pannel").src = "https://quickswap.exchange/#/quick"
-      console.log("Reward page");
-    };
+async function poolNavButtonClick() {
+  document.getElementById("right_pannel").src = "https://info.uniswap.org/#/pools";
+};
+    
+async function streamingNavButtonClick() {
+  document.getElementById("right_pannel").src = "stream.html";
+};
 
-    async function dclick() {
-      document.getElementById("right_pannel").src = "https://info.uniswap.org/#/"
-      console.log("overview page");
-    };
+/* Database */
 
-    async function eclick() {
-      document.getElementById("right_pannel").src = "https://info.uniswap.org/#/pools"
-      console.log("pool stats page");
-    };
+Moralis.initialize("A1wx0qFYX4M8QlKNVeC330wGq2EYnO0iOGQggXc8"); // Application id from moralis.io
+Moralis.serverURL = "https://kbcp7hj3liti.moralis.io:2053/server"; //Server url from moralis.io
 
-    async function fclick() {
-      document.getElementById("right_pannel").src = "https://info.uniswap.org/#/tokens"
-      console.log("Token stats page");
-    };
+async function login() {
+  console.log("login clicked");
+  var user = await Moralis.Web3.authenticate();
+  if (user) {
+    console.log(user);
+    user.save();
+  }
+}
 
-    Moralis.initialize("A1wx0qFYX4M8QlKNVeC330wGq2EYnO0iOGQggXc8"); // Application id from moralis.io
-    Moralis.serverURL = "https://kbcp7hj3liti.moralis.io:2053/server"; //Server url from moralis.io
-
-    async function login() {
-      console.log("login clicked");
-      var user = await Moralis.Web3.authenticate();
-      if (user) {
-        console.log(user);
-        user.save();
-      }
-    }
-
-    Moralis.User.enableUnsafeCurrentUser()
-    const currentUser = Moralis.User.current();
-    if (currentUser) {
-      console.log("log sucess");
-    } else {
-      console.log("unable to log");
-    }
+Moralis.User.enableUnsafeCurrentUser()
+const currentUser = Moralis.User.current();
+if (currentUser) {
+  console.log("log sucess");
+} else {
+  console.log("unable to log");
+}
